9ad8c7079259a91ea8f118d51a532ed1ec3217de
stroboskop <kamil.ostrovsky@gmail.com>
2017-07-26 12:56:19 +0200

    wersja stabilna

--- wysylanie_odbieranie_v1.ino
+++ wysylanie_odbieranie_v1.ino
@@ -4,11 +4,15 @@ const char* ssid     = "router";
 const char* password = "haslo123";
 
 const char* host = "192.168.1.101";
+WiFiServer server(80);
 
 String inData = "";
+String inDataState = "";
 String finalString = "";
 boolean boolReadSerial = true;
 boolean boolSend = false;
+
+
 void setup() {
   Serial.begin(115200);
   delay(10);
@@ -27,6 +31,7 @@ void setup() {
     Serial.print(".");
   }
 
+  server.begin();
   Serial.println("");
   Serial.println("WiFi connected");  
   Serial.println("IP address: ");
@@ -35,31 +40,84 @@ void setup() {
 }
 
 void loop() {
+  fromWiFi();
+  
   if(boolReadSerial == true && boolSend == false)
     serialRead();
 
   if(boolSend == true)
-    sendTom(finalString);
+    sendTom(inDataState, finalString);
+
   
 }
 
-void serialRead(){
-  if(Serial.available() >0){   
-      char inChar = Serial.read();      
-      
-      if(inChar == '\n'){
-      Serial.println("i mamy stringa:  " + inData);
-      inData.trim();
-      finalString = inData;
-      boolSend = true;   
-      }     
-      if(inChar != '\n' && inChar != '\r')
-      inData += inChar;
+void fromWiFi(){
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
   }
+  
+  // Wait until the client sends some data
+
+  while(!client.available()){
+    delay(1);
+  }
+  
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  Serial.println(req);
+  client.flush();
+  //client.stop();
+  
+  client.flush();
+
+  client.print("koniec");
+  delay(1);
 }
 
-void sendTom(String val){
-  boolReadSerial = false;
+int i = 1;
+
+void serialRead(){
+  if(Serial.available() >0){   
+      char inChar = Serial.read();   
+
+      if(inChar == '\n'){
+        Serial.println("i mamy stringa:  " + inData);
+        inData.trim();
+        finalString = String(inData[0]) + String(inData[1]) + String(inData[2]);
+        inDataState = inData[4];
+        Serial.println("nasz final: " + finalString);
+        Serial.println("nasz stan: " + inDataState);
+        boolSend = true;   
+      }     
+      if(inChar != '\n' && inChar != '\r')
+        inData += inChar;
+        
+ 
+       
+      
+//      if(inChar == '\n'){
+//      Serial.println("i mamy stringa:  " + inData);
+//      inData.trim();
+//      finalString = inData;
+//      boolSend = true;   
+//      }     
+//      if(inChar != '\n' && inChar != '\r' && inChar != ',' && i < 3)
+//      
+//      
+//      if(i == 4){
+//        inDataState = inChar;
+//        Serial.println("mamy cos: " + inDataState);
+//        i = 0;        
+//      }
+//      if(i < 4)
+//      i++;         
+//      }    
+}
+}
+
+void sendTom(String val, String valWhere){
+  boolReadSerial = false; 
   Serial.print("connecting to ");
   Serial.println(host);
 
@@ -73,7 +131,7 @@ void sendTom(String val){
   
   // We now create a URI for the request
   String url = "/test/test";
-  url += "?naz=podworko";
+  url += "?naz=" + valWhere;
   url += "&stan=" + val;
   url += "&kto=przechowalnia";
 
@@ -112,7 +170,6 @@ void sendTom(String val){
   Serial.println("closing connection");
   inData = "";
   finalString = "";
-  boolReadSerial = true;
-  boolSend = false;
+  i = 1;
 }
 
